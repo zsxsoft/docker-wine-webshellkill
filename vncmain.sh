d38d5191811214edf72086ec7b15c7a12c07b080
zsx <git@zsxsoft.com>
2019-01-09 19:33:41 +0800

    Submit

--- vncmain.sh
+++ vncmain.sh
@@ -7,29 +7,27 @@ export TERM=linux
 export LC_CTYPE=zh_CN.UTF-8
 export WINEDEBUG=-all
 
-cqexe=$(basename $(find ~/coolq -maxdepth 1 -type f -name '*.exe' | head -n 1))
+wskexe=$(basename $(find ~/webshellkill -maxdepth 1 -type f -name '*.exe' | head -n 1))
 
 while true; do
-    echo "[CQDaemon] Starting CoolQ ...."
-    wine ~/coolq/$cqexe /account $COOLQ_ACCOUNT &
-    echo "[CQDaemon] Started CoolQ ."
+    echo "[WSKDaemon] Starting WebShellKill ...."
+    wine ~/webshellkill/$wskexe &
+    echo "[WSKDaemon] Started WebShellKill ."
     wait
-    echo "[CQDaemon] CoolQ exited, maybe updated."
-    echo "[CQDaemon] Searching for the new process ..."
+    echo "[WSKDaemon] WebShellKill exited, maybe updated."
+    echo "[WSKDaemon] Searching for the new process ..."
     sleep 3
-    cqpid=$(ps x | grep $cqexe | head -n 1 | awk '{print $1}')
-    if [ "$cqpid" == "" ]; then
-        echo "[CQDaemon] No CoolQ process found, start new process ..."
+    wskpid=$(ps x | grep $wskexe | head -n 1 | awk '{print $1}')
+    if [ "$wskpid" == "" ]; then
+        echo "[WSKDaemon] No WebShellKill process found, start new process ..."
     else
-        echo "[CQDaemon] Found CoolQ process, it's okay."
-        tail -f /dev/null --pid=$cqpid
+        echo "[WSKDaemon] Found WebShellKill process, it's okay."
+        tail -f /dev/null --pid=$wskpid
     fi
-    # 酷Q 退出后直接重启 wine，然后重开。
-    # 因为酷Q 更新之类的会自己开回来，所以把整个 wine 干掉重启，比较靠谱
-    echo "[CQDaemon] CoolQ exited. Killing wine ..."
+    echo "[WSKDaemon] WebShellKill exited. Killing wine ..."
     sleep 1
     wine wineboot --kill
     wineserver -k9
-    echo "[CQDaemon] CoolQ will start after 3 seconds ..."
+    echo "[WSKDaemon] WebShellKill will start after 3 seconds ..."
     sleep 3
 done
